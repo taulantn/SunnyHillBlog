56077756f7d152a0da3c079328e27758239081bc
taulantn <tn47423@ubt-uni.net>
2022-03-02 03:19:32 +0100

    Created BlogPost and import to Home

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>Hello</h2>
+    </div>
+</template>
+<script>
+    export default{
+        name: "blogPost",
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
